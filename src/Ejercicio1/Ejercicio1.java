bb14595d1e194fa50038b7847c8b13e8013513aa
Nacho <nacho>
2020-02-18 10:11:42 +0100

    Guardado2

--- src/Ejercicio1/Ejercicio1.java
+++ src/Ejercicio1/Ejercicio1.java
@@ -9,14 +9,32 @@ public class Ejercicio1 {
 		
 		Persona p = new Persona();
 		
+		Persona p2 = new Persona();
+		
 		Scanner keyboard = new Scanner(System.in);
 		
 		ConsoleInput consola = new ConsoleInput(keyboard);
 		
 		System.out.println("Introduce un nombre: ");
 		p.setNombre(consola.readString());
+		System.out.println("Introduce una edad: ");
+		p.setEdad(consola.readInt());
+		System.out.println("Introduce una altura: ");
+		p.setAltura(consola.readFloat());
+		System.out.println("Introduce una ocupacion: ");
+		p.setOcupacion(consola.readString());
 		
-
+		
+		System.out.println("Persona 2: ");
+		p2.setNombre("Guillermo");
+		p2.setEdad(27);
+		p2.setAltura(1.82f);
+		p2.setOcupacion("Carcelero");
+		
+		System.out.println("Nombre: " + p2.getNombre());
+		System.out.println("Edad: " + p2.getEdad());
+		System.out.println("Altura: " + p2.getAltura());
+		System.out.println("Ocupacion: " + p2.getOcupacion());
 	}
 
 }
--- src/Ejercicio1/Persona.java
+++ src/Ejercicio1/Persona.java
@@ -6,6 +6,15 @@ public class Persona {
 	private int edad;
 	private float altura;
 	private String ocupacion;
+	private int sueldo;
+	
+	int getSueldo() {
+		return sueldo;
+	}
+	
+	void setSueldo(int sueldo) {
+		this.sueldo=sueldo;
+	}
 	
 	String getNombre() {
 		return nombre;
@@ -15,6 +24,67 @@ public class Persona {
 		this.nombre=nombre;
 	}
 	
+	int getEdad() {
+		return edad;
+	}
+	
+	void setEdad(int edad) {
+		this.edad=edad;
+	}
+	
+	float getAltura() {
+		return altura;
+	}
+	
+	void setAltura(float altura) {
+		this.altura=altura;
+	}
+	
+	String getOcupacion() {
+		return ocupacion;
+	}
+	
+	void setOcupacion(String ocupacion) {
+		this.ocupacion=ocupacion;
+	}
+	
+	public Persona(String nombre, int edad, float altura, String ocupacion, int sueldo) {
+		this.nombre=nombre;
+		this.edad=edad;
+		this.altura=altura;
+		this.ocupacion=ocupacion;
+		this.sueldo=sueldo;
+	}
+	
+	public Persona() {
+		this("Noelia",21,1.60f,"biologa",1800);
+	}
+	
+	void sumarSueldo(Persona p) {
+		sueldo+=p.sueldo;
+	}
+	
+	void doblarSueldo (Persona p) {
+		sumarSueldo(this);
+	}
+	
+	public static void main(String[] args) {
+		
+		Persona p = new Persona();
+		
+		System.out.printf("Nombre: %s",p.getNombre());
+		System.out.printf(" Edad: %d",p.getEdad());
+		System.out.printf(" Altura: %.1f",p.getAltura());
+		System.out.printf(" Ocupacion: %s",p.getOcupacion());
+		p.sumarSueldo(p);
+		p.doblarSueldo(p);
+		System.out.printf(" Sueldo: %d",p.getSueldo());
+		
+
+		
+		
+		
+	}
 	
 	
 	
