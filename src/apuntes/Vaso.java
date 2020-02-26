0ced34932e0c534e049b4fb6e6fad547af42908e
Nacho <nacho>
2020-02-26 13:37:03 +0100

    Guardado8

--- src/apuntes/Vaso.java
+++ src/apuntes/Vaso.java
@@ -2,7 +2,7 @@ package apuntes;
 
 public enum Vaso {
 	
-	JARRA(500,20.7f), TUBO(250,16.8f), TERCIO(333,12.3f), CAÑA(200,8.5f);
+	JARRA(500,20.7f), TUBO(250,16.8f), TERCIO(333,12.3f), CAnA(200,8.5f);
 	
 	private int cc; 
 	private float altura;
